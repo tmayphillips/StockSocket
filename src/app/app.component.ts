87a32df5d9af5538b9f78db25ae5965bf1e02402
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2022-01-05 15:29:43 -0600

    Client connected to server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SocketService } from './socket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'StockSocket';
+
+  constructor(private socketService: SocketService) {  }
+
+  ngOnInit() {
+    // this.socketService.getStockList().subscribe((data: any) => console.log(data));
+    this.socketService.getStockList().subscribe((data) => {
+      console.log(data);
+    })
+  }
 }
--- src/app/socket.service.ts
+++ src/app/socket.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {io, Socket} from 'socket.io-client';
+import { io, Socket }from 'socket.io-client';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
@@ -8,14 +8,29 @@ import { BehaviorSubject, Observable } from 'rxjs';
 export class SocketService {
 
   private url = 'http://localhost:4000'
-  private socket: Socket = io(this.url)
+  private socket: Socket;
 
   public stockList$: BehaviorSubject<string> = new BehaviorSubject('');
 
-  constructor() { }
+  constructor() {
+    this.socket = io('http://localhost:4000');
+   }
+
+  // listen(eventName: string) {
+  //   return new Observable((subscriber) => {
+  //     this.socket.on(eventName, (data) => {
+  //       subscriber.next(data)
+  //     })
+  //   })
+  // }
+
+  // emit(eventName: string, data: any) {
+  //   this.socket.emit(eventName, data);
+  // }
 
   public getStockList = () => {
     this.socket.on('list', (data) => {
+      console.log('subscribe');
       this.stockList$.next(data)
     })
     return this.stockList$.asObservable();
