826d83c1e55f290af73b024db72937d53950561d
Tiffany May-Phillips <tiffany.phillips12@gmail.com>
2022-01-05 12:30:37 -0600

    stockList

--- src/app/socket.service.ts
+++ src/app/socket.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import io from 'socket.io-client'
+import {io, Socket} from 'socket.io-client';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +8,16 @@ import io from 'socket.io-client'
 export class SocketService {
 
   private url = 'http://localhost:4000'
-  private socket: any
+  private socket: Socket = io(this.url)
+
+  public stockList$: BehaviorSubject<string> = new BehaviorSubject('');
 
   constructor() { }
+
+  public getStockList = () => {
+    this.socket.on('list', (data) => {
+      this.stockList$.next(data)
+    })
+    return this.stockList$.asObservable();
+  }
 }
